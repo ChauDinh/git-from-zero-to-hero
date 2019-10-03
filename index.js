52f7f6c9c5cbbd303d957dcd536df658e99d3af6
Chau Dinh <chaudvb95uc@gmail.com>
2019-10-04 04:55:31 +0700

    Add methods for Cat

--- index.js
+++ index.js
@@ -5,12 +5,25 @@ function Dog(name) {
   this.stomach = [];
 }
 
+function Cat(name) {
+  this.name = name;
+  this.stomach = [];
+}
+
 Dog.prototype.eat = function(cat) {
   this.stomach.push(cat);
 };
 
 Dog.prototype.sayHi = function() {
   console.log('Hi, I am a dog, my name is ' + chalk.red(this.name));
+};
+
+Cat.prototype.eat = function(dog) {
+  this.stomach.push(dog);
+};
+
+Cat.prototype.sayHi = function() {
+  console.log('Hi, I am a cat, my name is ' + chalk.green(this.name));
 };
 
 module.exports = Dog;

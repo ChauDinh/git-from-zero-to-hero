647ff970477cab9329cedbedf91940d293141ab8
Chau Dinh <chaudvb95uc@gmail.com>
2019-10-04 04:41:06 +0700

    sayHi method for Dog

---
+++ index.js
@@ -0,0 +1,16 @@
+var chalk = require('chalk');
+
+function Dog(name) {
+  this.name = name;
+  this.stomach = [];
+}
+
+Dog.prototype.eat = function(cat) {
+  this.stomach.push(cat);
+};
+
+Dog.prototype.sayHi = function() {
+  console.log('Hi, I am a dog, my name is ' + chalk.red(this.name));
+};
+
+module.exports = Dog;
---
+++ server.js
@@ -0,0 +1,4 @@
+var Dog = require('./index');
+
+var dog1 = new Dog('Tom');
+dog1.sayHi();

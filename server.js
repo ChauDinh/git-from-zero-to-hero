e566b5e0350a49bbb2994b3390160e1206fa39df
Chau Dinh <chaudvb95uc@gmail.com>
2019-10-04 17:13:15 +0700

    add getPoint and checkComplete methods for LeetCode

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 var Dog = require('./index');
 
+var LeetCode = require('./leetcode');
+
 var dog1 = new Dog('Tom');
 dog1.sayHi();
+
+var leetcode1 = new LeetCode();
+
+leetcode1.getPoint(true);
+leetcode1.checkComplete();
+
+console.log(leetcode1.points);

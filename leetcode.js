439066e19183665a3a402b69e8548bba44f0b532
Chau Dinh <chaudvb95uc@gmail.com>
2019-10-04 17:02:20 +0700

    add leetcode features

--- leetcode.js
+++ leetcode.js
@@ -2,4 +2,25 @@
  * This is a collection of challenges from LeetCode
  */
 
+function LeetCode() {
+  this.isComplete = false;
+  this.points = 0;
+}
 
+LeetCode.prototype.getPoint = function(answer) {
+  if (answer) {
+    this.points = this.points + 1;
+    return;
+  }
+  this.points = thisn.points - 1;
+};
+
+LeetCode.prototype.checkComplete = function() {
+  if (this.points > 10) {
+    this.isComplete = true;
+    return;
+  }
+  this.isComplete = false;
+};
+
+module.exports = LeetCode;
